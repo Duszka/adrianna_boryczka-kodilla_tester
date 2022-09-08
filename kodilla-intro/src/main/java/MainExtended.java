f2412f4813e316ea4adac584a031f41210211968
Adrianna Boryczka <adriannaboryczka@gmail.com>
2022-09-08 09:45:10 +0200

    exercise deklarowanie i wywoływanie zmiennych

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String breed = "Seter Irlandzki";
+        String type = "pies myśliwski";
+        String colour = "mahoniowy";
+        int mediumHeight = (65);
+        int mediumWeight = (29);
+        boolean hasFur = false;
+        System.out.println(breed);
+        System.out.println(type);
+        System.out.println(colour);
+        System.out.println(mediumHeight);
+        System.out.println(mediumWeight);
+        System.out.println(hasFur);
+    }
+}
